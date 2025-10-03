22e515489d01081eaa8fdb6d9384b383ec8d1e99
MaxSummerX <marspowerpop@gmail.com>
2025-10-03 20:36:13 +0300

    new route in note.py -> read_note

--- app/note.py
+++ app/note.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 
 
 router = APIRouter()
@@ -7,5 +7,15 @@ notes_db = {0: "Study FastAPI", 1: "I like FastAPI"}
 
 
 @router.get("/notes", tags=["notes"])
-async def read_notes() -> dict:
+async def read_notes() -> dict[int, str]:
+    if not notes_db:
+        return {}
     return notes_db
+
+
+@router.get("/notes/{note_id}", tags=["notes"])
+async def read_note(note_id: int) -> str:
+    try:
+        return notes_db[note_id]
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
